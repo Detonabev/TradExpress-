0c53503506f2c66883917d7f5b4773f15116b9a2
Dara <Detonabev@gmail.com>
2022-11-18 19:20:18 +0100

    TradExpress Landing Page updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,59 +1,62 @@
 import React from 'react'
 import '../Styles/Footer.css'
-import {TfiTwitter} from 'react-icons/tfi'
-import {TbBrandTelegram} from 'react-icons/tb'
-import {ImWhatsapp} from 'react-icons/im'
-import {BsFacebook} from 'react-icons/bs'
+import { TfiTwitter } from 'react-icons/tfi'
+import { TbBrandTelegram } from 'react-icons/tb'
+import { ImWhatsapp } from 'react-icons/im'
+import { BsFacebook } from 'react-icons/bs'
 
 
 const Footer = () => {
-  return (
-    <div>
-        <div className='footer'>
-        <div className='products'>
-            <p  className='footer-text' >Products</p> 
-            <a className='products-a' href="#top">Bitcoin</a> <br />
-            <a className='products-a' href="#top">Alt</a> <br />
-            <a className='products-a' href="#top">Fiat</a> <br />
-            <a className='products-a' href="#top">Refill</a> <br />
-            <a className='products-a' href="#top">P2P</a> 
-        </div>
+    return (
+        <div>
+            <div className='footer'>
+                <div className='products'>
+                    <p className='footer-text' >Products</p>
+                    <a className='products-a' href="#top">Bitcoin</a> <br />
+                    <a className='products-a' href="#top">Alt</a> <br />
+                    <a className='products-a' href="#top">Fiat</a> <br />
+                    <a className='products-a' href="#top">Refill</a> <br />
+                    <a className='products-a' href="#top">P2P</a>
+                </div>
 
-        <div className='learn'>
-            <p className='footer-text'>Learn</p>
-            <a className='learn-a' href="#top">Blog</a> <br />
-            <a className='learn-a' href="#top">Help Center</a>
-        </div>
+                <div className='learn'>
+                    <p className='footer-text'>Learn</p>
+                    <a className='learn-a' href="#top">Blog</a> <br />
+                    <a className='learn-a' href="#top">Help Center</a>
+                </div>
 
-        <div className='contact'>
-            <p className='footer-text'>Contact</p>
-            <a className='contact-a' href="#top">hello@tradeexpress.com</a> <br />
-            <a className='contact-a' href="#top">support@tradeexpress.com</a> <br />
-            <div className='icons'>
-            <TfiTwitter/>
-            <TbBrandTelegram/>
-            <ImWhatsapp/>
-            <BsFacebook/>
+                <div className='contact'>
+                    <p className='footer-text'>Contact</p>
+                    <a className='contact-a' href="#top">hello@tradeexpress.com</a> <br />
+                    <a className='contact-a' href="#top">support@tradeexpress.com</a> <br />
+                    <div className='icons'>
+                        <TfiTwitter />
+                        <TbBrandTelegram />
+                        <ImWhatsapp />
+                        <BsFacebook />
+                    </div>
+                </div>
+
+                <div className='company'>
+                    <p className='footer-text'>Company</p>
+                    <a className='company-a' href="#top">About Us</a><br />
+                    <a className='company-a' href="#top">Careers</a><br />
+                    <a className='company-a' href="#top">Rates</a><br />
+                    <a className='company-a' href="#top">Mobile</a>
+                </div>
+
+                <div className='legal'>
+                    <p className='footer-text'>Legal</p>
+                    <a className='legal-a' href="#top">Privacy Policy</a> <br />
+                    <a className='legal-a' href="#top">Anti-Money Laundering</a> <br />
+                    <a className='legal-a' href="#top">Terms and Conditions</a> <br />
+                </div>
+            </div>
+            <div className='copyright'>
+               <p> © 2021 TradExpress Technologies All Rights Reserved</p>
             </div>
         </div>
-
-        <div className='company'>
-            <p className='footer-text'>Company</p>
-            <a className='company-a' href="#top">About Us</a><br />
-            <a className='company-a' href="#top">Careers</a><br />
-            <a className='company-a' href="#top">Rates</a><br />
-            <a className='company-a' href="#top">Mobile</a>
-        </div>
-
-        <div className='legal'>
-            <p className='footer-text'>Legal</p>
-            <a className='legal-a' href="#top">Privacy Policy</a> <br />
-            <a className='legal-a' href="#top">Anti-Money Laundering</a> <br />
-            <a className='legal-a' href="#top">Terms and Conditions</a> <br />
-        </div>
-    </div>
-    </div>
-  )
+    )
 }
 
 export default Footer
